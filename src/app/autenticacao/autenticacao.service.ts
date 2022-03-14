eb29e14e29d35f845c5a8510084ec999c3720f39
Bruno Staine <brunostaine@hotmail.com>
2022-03-14 15:05:07 -0300

    header da aplicação

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -7,6 +7,7 @@ import { UsuarioService } from './usuario/usuario.service';
 @Injectable({
   providedIn: 'root',
 })
+
 export class AutenticacaoService {
 
   constructor(
@@ -14,15 +15,19 @@ export class AutenticacaoService {
     private usuarioService: UsuarioService) { }
 
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
+    return this.httpClient.post('http://localhost:3000/user/login', 
+    {
       userName: usuario,
       password: senha,
     },
-    { observe: 'response'}
-  ).pipe (
+    { observe: 'response' }
+  )
+  .pipe (
     tap((res) => {
       const authToken = res.headers.get('x-access-token') ?? '';
       this.usuarioService.salvaToken(authToken);
     })
+    )
   } 
 }
+
