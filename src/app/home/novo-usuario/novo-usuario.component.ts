44208a6431a7fc30d2fa60700765b9cc920e2a9e
Bruno Staine <brunostaine@hotmail.com>
2022-03-14 11:08:45 -0300

    validação padrão do email e full name

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service'; // não esta sendo usado por enquanto, fiz uma gambiarra para o código voltar. No app.modules adicionei o serviço NovoUsuarioService e importei ele tirando o erro provisóriamente
-
-
 
 @Component({
   selector: 'app-novo-usuario',
@@ -20,8 +18,8 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]], 
       userName: [''],
       password: [''],
     })
