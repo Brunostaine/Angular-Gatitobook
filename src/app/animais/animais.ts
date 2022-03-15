1623387b1b2c5a011ab1b25843864a900cb3214f
Bruno Staine <brunostaine@hotmail.com>
2022-03-15 15:25:44 -0300

    :sparkles: Criado a interface de animais e o serviço de animais

---
+++ src/app/animais/animais.ts
@@ -0,0 +1,14 @@
+import { type } from "os";
+
+export interface Animal {
+    id: number;
+    postDate: Date;
+    url: string;
+    description: string;
+    allowComments: boolean;
+    likes: number;
+    comments: number;
+    userId: number;
+}
+
+export type Animais = Array<Animal>
