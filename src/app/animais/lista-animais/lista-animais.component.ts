564440153324f20d6f0aafa7f5239b864cec923f
Bruno Staine <brunostaine@hotmail.com>
2022-03-15 16:56:07 -0300

    :sparkles: arrumando o bootstrap e colocando as primeiras fotos

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  constructor() { }
+  animais!: Animais;
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) { }
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      })
+    })
   }
 
 }
