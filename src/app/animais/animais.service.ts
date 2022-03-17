c4a87f29bf9c9646d4d952d716eea3845dd77817
Bruno Staine <brunostaine@hotmail.com>
2022-03-17 16:18:34 -0300

    :sparkles: método de curtir e excluir foto

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, mapTo, Observable, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
 import { Animais, Animal } from './animais';
 
-  const API = environment.apiURL;
+const API = environment.apiURL;
+const NOT_MODIFIED = '304'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,5 +21,20 @@ export class AnimaisService {
 
   buscaPorID(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`)
+  }
+
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`)
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http
+    .post(`${API}/photos/${id}/like`, {}, {observe: 'response'})
+    .pipe(
+      mapTo(true),
+      catchError((error)=> {
+      return error.status === NOT_MODIFIED ? of(false) : throwError(error)
+    })
+    )
   }
 }
