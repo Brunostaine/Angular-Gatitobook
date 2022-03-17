0003ca5cfb4d31424f20af30d1ed39b954cbec4f
Bruno Staine <brunostaine@hotmail.com>
2022-03-17 11:36:02 -0300

    :sparkles: Adicionando guarda rotas no Home e na página animais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
 const routes: Routes = [
   {
@@ -11,11 +13,13 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import (`./home/home.module`).then((m)=> m.HomeModule),
+    canLoad: [LoginGuard],
   },
   {
     path: 'animais',
     loadChildren: () => import (`./animais/animais.module`).then((m) => m.AnimaisModule),
-  }
+    canLoad: [AutenticacaoGuard],
+  },
 ];
 
 @NgModule({
