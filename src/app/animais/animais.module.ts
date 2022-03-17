668e51813003c13dc1c9aca0e36bea3e169137c2
Bruno Staine <brunostaine@hotmail.com>
2022-03-17 12:18:08 -0300

    :sparkles: Component detalhe animal

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -6,13 +6,15 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 
 @NgModule({
   declarations: [
     ListaAnimaisComponent,
     AnimalComponent,
-    GradeFotosAnimaisComponent
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent
   ],
   imports: [
     CommonModule,
